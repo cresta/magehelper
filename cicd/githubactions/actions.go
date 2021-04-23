3e90bc90415a91d63fb94a99a5aca02dd039c592
Jack Lindamood <jack@cresta.ai>
2021-04-22 17:28:38 -0700

    allow gh actions to free up space (#6)

--- cicd/githubactions/actions.go
+++ cicd/githubactions/actions.go
@@ -1,8 +1,14 @@
 package githubactions
 
 import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+
 	"github.com/cresta/magehelper/cicd"
 	"github.com/cresta/magehelper/env"
+	"github.com/cresta/magehelper/pipe"
 )
 
 type GithubActions struct {
@@ -47,4 +53,27 @@ func (g *GithubActions) Name() string {
 
 func (g *GithubActions) GitRepository() string {
 	return g.Env.Get("GITHUB_REPOSITORY")
+}
+
+// FreeDiskSpace will free up space on disk.  There is a lot of cruft on the github actions runners.  Here are a few
+// interesting links:
+//   * https://github.com/ThewBear/free-actions
+//   * https://github.com/actions/virtual-environments/issues/709
+//   * https://github.com/NickleDave/vak/issues/341
+//   * https://github.com/flashlight/wav2letter/actions/runs/74797824/workflow
+//   * https://github.com/search?q=%22rm+-rf+%2Fusr%2Fshare%2Fdotnet%22&type=code
+func FreeDiskSpace(ctx context.Context) error {
+	if _, isGh := cicd.Instance().(*GithubActions); !isGh {
+		return fmt.Errorf("cicd is not set to github actions: %s", cicd.Instance().Name())
+	}
+	if os.Getenv("CI") != "true" || os.Getenv("GITHUB_ACTIONS") != "" {
+		return errors.New("do not appear to be running inside github actions")
+	}
+	if err := pipe.NewPiped("df", "-h").Run(ctx); err != nil {
+		return err
+	}
+	if err := pipe.NewPiped("rm", "-rf", "/usr/share/dotnet", "/usr/local/lib/android", "/usr/share/swift").Run(ctx); err != nil {
+		return err
+	}
+	return pipe.NewPiped("df", "-h").Run(ctx)
 }
