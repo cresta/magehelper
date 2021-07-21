0ff9731e1670af2c9deab0a5c45719593db78b63
Steven Callister <steven@cresta.ai>
2021-07-20 17:58:15 -0700

    Updating with Jacks feedback

--- docker/docker.go
+++ docker/docker.go
@@ -152,39 +152,38 @@ func (d *Docker) Tag() string {
 // If DOCKER_LATEST_BRANCH is not defined, returns "main"
 // If branch "main" does not exist in repository, returns "master"
 // On any error, returns "master" to maintain original compatibility
-func (d *Docker) latestBranch() (latestBranch string) {
+func (d *Docker) latestBranch() string {
 	// If a user sets DOCKER_LATEST_BRANCH that should always be respected
-	latestBranch = d.Env.Get("DOCKER_LATEST_BRANCH")
-	if latestBranch != "" {
-		return
+	dockerLatestBranch := d.Env.Get("DOCKER_LATEST_BRANCH")
+	if dockerLatestBranch != "" {
+		return dockerLatestBranch
 	}
 	// Leaving "master" as default to maintain compatibility if checking git repo fails
-	latestBranch = "master"
+	oldDefaultBranch := "master"
 	repo, err := gogit.PlainOpen(".")
 	if err != nil {
-		return
+		return oldDefaultBranch
 	}
 	branches, err := repo.Branches()
 	if err != nil {
-		return
+		return oldDefaultBranch
 	}
 
 	// Return main if it exists, otherwise return master to maintain compatibility
 	for {
 		branch, err := branches.Next()
 		if err != nil {
-			return
+			return oldDefaultBranch
 		}
 		if branch == nil {
 			break
 		}
 		// Return "main" if that branch exists
 		if branch.Name().String() == "main" {
-			latestBranch = branch.Name().String()
-			return
+			return "main"
 		}
 	}
-	return
+	return oldDefaultBranch
 }
 
 //  If requireOnlyOne is set, then at most one result is returned. THis resolves a limitation of --cache-to
