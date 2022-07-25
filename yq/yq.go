223d7fa6c615a873ff691ec10665e66d67e7f2bc
Jack Lindamood <jack@cresta.ai>
2022-07-25 11:48:42 -0700

    extra commands

--- yq/yq.go
+++ yq/yq.go
@@ -1,11 +1,14 @@
 package yq
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"github.com/cresta/magehelper/files"
 	"github.com/cresta/magehelper/pipe"
 	"path/filepath"
+	"regexp"
+	"strings"
 )
 
 type Yq struct{}
@@ -16,6 +19,21 @@ func (y *Yq) Reformat(ctx context.Context, path string) error {
 	err := pipe.NewPiped("yq", "-P", "-i", path).Run(ctx)
 	if err == nil {
 		return fmt.Errorf("unable to reformat: %w", err)
+	}
+	return nil
+}
+
+func (y *Yq) VersionCheck(ctx context.Context) error {
+	var out bytes.Buffer
+	err := pipe.NewPiped("yq", "--version").Execute(ctx, nil, &out, nil)
+	if err != nil {
+		return fmt.Errorf("unable to check version: %w", err)
+	}
+	version := strings.TrimSpace(out.String())
+	rgx := `mikefarah.* version 4\..`
+	cmp := regexp.MustCompile(rgx)
+	if !cmp.MatchString(version) {
+		return fmt.Errorf("must match version 4 (%s), saw -- %s", rgx, version)
 	}
 	return nil
 }
@@ -32,4 +50,19 @@ func (y *Yq) ReformatYAMLDir(ctx context.Context, root string) error {
 		}
 	}
 	return nil
+}
+
+// Reformat all YAML files in PATH with YQ
+func Reformat(ctx context.Context, path string) error {
+	return Instance.Reformat(ctx, path)
+}
+
+// VersionCheck checks the version of YQ to match 4.x
+func VersionCheck(ctx context.Context) error {
+	return Instance.VersionCheck(ctx)
+}
+
+// ReformatYAMLDir reformats all YAML files in PATH with YQ
+func ReformatYAMLDir(ctx context.Context, root string) error {
+	return Instance.ReformatYAMLDir(ctx, root)
 }
